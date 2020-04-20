95fadf54e87609d2b54d800b1ad7809f2915486b
raghav7203 <48362662+raghav7203@users.noreply.github.com>
2020-04-20 15:05:56 +0530

    partial_scrapping

---
+++ scrap_partial.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import json
+import requests
+import csv 
+
+
+solditems = requests.get('https://api.covid19india.org/data.json') # (your url)
+data = solditems.json()
+with open('data.json', 'w') as f:
+    json.dump(data, f,indent=4)
+
+with open('data.json') as json_file: 
+    data = json.load(json_file) 
+cases=data['cases_time_series']
+
+data_file = open('data_file.csv', 'w') 
+  
+# create the csv writer object 
+csv_writer = csv.writer(data_file) 
+  
+# Counter variable used for writing  
+# headers to the CSV file 
+count = 0
+  
+for case in cases: 
+    if count == 0: 
+  
+        # Writing headers of CSV file 
+        header = case.keys() 
+        csv_writer.writerow(header) 
+        count += 1
+  
+    # Writing data of CSV file 
+    csv_writer.writerow(case.values()) 
+  
+data_file.close() 
+
+f=pd.read_csv("data_file.csv")
+keep_col = ['totalconfirmed','totaldeceased']
+new_f = f[keep_col]
+new_f.to_csv("newFile.csv", index=False)
